39ff9a5bc4b77726ff4ebf1c65eb8f736ec691d1
tanmay2626 <87329832+tanmay2626@users.noreply.github.com>
2022-05-27 18:46:39 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './css/App.css';
+import Navigation from './comp/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+    <Navigation />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comp/Navigation.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import { Button } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import '../css/Navigation.css'
+import { InputBase } from '@mui/material';
+
+
+
+const Navigation = () => {
+
+  return (
+    <Box className='navigation' sx={{ flexGrow: 1 }}>
+      <AppBar sx={{ backgroundColor: '#131921', padding: 0 }} className='navigation'  position="static">
+        <Toolbar>
+         <img src='https://github.com/tanmay2626/images/blob/main/amazon-logo-removebg-preview.png?raw=true' alt='logo' />
+         
+         <Box sx={{ marginLeft: 5 , width: 65+'%'}}>
+         <InputBase sx={{ padding:1, backgroundColor: 'white' , borderRadius: '3px 0 0 3px' , width: 90+'%', height: 45+'px' }} />
+         <IconButton sx={{ backgroundColor: '#E48900' ,padding: 0 ,borderRadius: '0 3px 3px 0' }}>
+           <SearchIcon sx={{  color: 'black', padding: 1}} />
+         </IconButton>
+         </Box>
+          <Box sx={{ display: { xs: 'none', md: 'flex', width: 30+'%', height: 50+'px' } }}>
+            <Button sx={{ textTransform: 'none' , overflow: 'hidden'}} variant="text"   color="inherit">
+            <p><span>Hello Guest</span>, Sign in</p>
+            </Button>
+            <Button sx={{ textTransform: 'none' }} variant="text"  color="inherit">
+            <p><span>Returns </span>& Orders</p>
+            </Button>
+            <IconButton sx={{ textTransform: 'none' }} variant="text" color="inherit">
+            <ShoppingCartOutlinedIcon sx={{ fontSize: 35 }} />
+            <p>Cart</p>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Navigation
