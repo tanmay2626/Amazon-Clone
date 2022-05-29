dfc3895324691b8aef7c3d78edf0c205be0bc56d
tanmay2626 <87329832+tanmay2626@users.noreply.github.com>
2022-05-29 11:08:56 +0530

    add id field in home

--- src/data/home.js
+++ src/data/home.js
@@ -1,12 +1,14 @@
 
 const rowA = [
-    {
+    {   
+        id: '814jC+rODgL._AC_UY218_',
         product_name: 'The Subtle Art of Not Giving a F*ck: A Counterintuitive Approach to Living a Good Life',
         price: 303,
         rating: 4.5,
         img_url: 'https://m.media-amazon.com/images/I/814jC+rODgL._AC_UY218_.jpg'
     },
     {
+        id: '71fVoqRC0wL._AC_UY218_',
         product_name: 'Apple iPhone 12 (128GB) - Black',
         price: 60900,
         rating: 4.5,
@@ -16,18 +18,21 @@ const rowA = [
 
 const rowB = [
     {
+        id: '81ewnfSWrrL._AC_UL320_',
         product_name: 'Yonex Nanoray Light 18i Graphite Badminton Racquet (77g, 30 lbs Tension)',
         price: 1798,
         rating: 5,
         img_url: 'https://m.media-amazon.com/images/I/81ewnfSWrrL._AC_UL320_.jpg'
     },
     {
+        id: '51jTBUXhnkL._AC_UY218_',
         product_name: 'Dell New Inspiron 5418 Laptop Intel i5-11320H, 16GB DDR4, 512Gb SSD, Win 11 + MS Office 21, 14" (35.56 cms) FHD 250 Nits Display, Platinum Silver, FPR + Backlit KB (D560633WIN9S), 1.46Kgs 4.1 out of 5 stars 77',
         price: 66690,
         rating: 4,
         img_url: 'https://m.media-amazon.com/images/I/51jTBUXhnkL._AC_UY218_.jpg'
     },
     {
+        id: '31Y3d2IJfxL._AC_UL320_',
         product_name: 'Nike Unisex Revolution 5 (Gs) Black/Oracle Aqua-Hyper Blue-Ghost Green Running Shoes-5.5 UK (38.5 EU) (6 Kids US) (BQ5671-011)',
         price: 2464,
         rating: 4.5,
@@ -37,36 +42,42 @@ const rowB = [
 
 const rowC = [
     {
+        id: '71ZKGDdz1lL._SX450_',
         product_name: 'Sony Bravia 164 cm (65 inches) 4K Ultra HD Smart LED Google TV KD-65X74K (Black) (2022 Model) | with Alexa Compatibility',
         price: 96990,
         rating: 4.5,
         img_url: 'https://m.media-amazon.com/images/I/71ZKGDdz1lL._SX450_.jpg'
     },
     {
+        id: '914hFeTU2-L._AC_UL320_',
         product_name: 'Canon EOS 1500D 24.1 Digital SLR Camera (Black) with EF S18-55 is II Lens',
         price: 36499,
         rating: 4.5,
         img_url: 'https://m.media-amazon.com/images/I/914hFeTU2-L._AC_UL320_.jpg'
     },
     {
+        id: '51LVX01HGNL._AC_UL320_',
         product_name: 'Philips Hue 9.5W E27 Smart LED Light Bulb (White Ambiance), Compatible with Amazon Alexa, Apple HomeKit, and The Google Assistant',
         price: 2375,
         rating: 4,
         img_url: 'https://m.media-amazon.com/images/I/51LVX01HGNL._AC_UL320_.jpg'
     },
     {
+        id: '61w8gouYvxL._SX569_',
         product_name: 'Echo Dot (3rd Gen) with Amazon Smart Plug',
         price: 4298,
         rating: 4.5,
         img_url: 'https://m.media-amazon.com/images/I/61w8gouYvxL._SX569_.jpg'
     },
     {
+        id: '61IMRs+o0iL._AC_UY218_',
         product_name: 'boAt Xtend Smartwatch with Alexa Built-in, 1.69” HD Display, Multiple Watch Faces, Stress Monitor, Heart & SpO2 Monitoring, 14 Sports Modes, Sleep Monitor & 5 ATM Water Resistance(Pitch Black)',
         price: 3399,
         rating: 4,
         img_url: 'https://m.media-amazon.com/images/I/61IMRs+o0iL._AC_UY218_.jpg'
     },
     {
+        id: '61PqVhNyKuL._AC_UY218_',
         product_name: 'OnePlus Nord Buds |True Wireless Earbuds| 12.4mm Titanium Drivers | Playback:Up to 30hr case | 4-Mic Design + AI Noise Cancellation| IP55 Rating |Fast Charging: 10min for 5hr Playback (Black Slate)||',
         price: 2799,
         rating: 4.5,
