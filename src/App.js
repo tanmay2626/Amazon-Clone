37a27e46db58b85037899ea397af2567dfdca4dc
tanmay2626 <87329832+tanmay2626@users.noreply.github.com>
2022-05-29 11:27:10 +0530

    setup react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import React from 'react'
 import './css/App.css';
 import Navigation from './comp/Navigation';
 import Home from './comp/Home';
 import Checkout from './comp/Checkout';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-    <Navigation />
-    <Home />
-    {/* <Checkout /> */}
+      <Navigation />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/cart' exact element={<Checkout />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
