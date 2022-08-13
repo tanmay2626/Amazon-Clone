ff2d3c71c1eed90434947407e4e7b2682fc5955a
tanmay2626 <87329832+tanmay2626@users.noreply.github.com>
2022-08-13 10:13:04 +0530

    start a new design

--- client/src/comp/Home.js
+++ client/src/comp/Home.js
@@ -1,75 +1,61 @@
-import { Box, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import React from "react";
 import "../css/Home.css";
 import { rowA, rowB, rowC } from "../data/products";
 import Product from "./Product";
+import SpeakerOutlinedIcon from '@mui/icons-material/SpeakerOutlined';
 
 const Home = (props) => {
+
+  const categories = [
+    {
+      name: 'Amazon Devices'
+    },
+    {
+      name: 'Back to School'
+    },
+    {
+      name: 'Computers'
+    },
+    {
+      name: 'Electronics'
+    },
+    {
+      name: 'Kitchen'
+    },
+    {
+      name: 'Toys & Games'
+    },
+  ]
+
   return (
     <div className="home">
       {/* Todo - add slider */}
-      <Box className="banner">
+      <div className="home-left">
+      <div className="banner">
         <img
           alt="banner"
-          src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/HP_Gaming_Acc/Hero/HP_DesktopTallHero_3000x1200._CB623691740_.jpg"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDJ8IYSKSlGR7OjqZZCwnnk_IqFHBco4lydg&usqp=CAU"
         />
-      </Box>
-      <div className="home_row_a">
-        {rowA.map((item, index) => {
-          return (
-            <Product
-              key={index}
-              product_id={item.product_id}
-              name={item.product_name}
-              rating={item.rating}
-              img={item.img_url}
-              price={item.price}
-            />
-          );
-        })}
       </div>
-      <div className="home_row_b">
-        {rowB.map((item, index) => {
-          return (
-            <Product
-              key={index}
-              product_id={item.product_id}
-              name={item.product_name}
-              rating={item.rating}
-              img={item.img_url}
-              price={item.price}
-            />
-          );
-        })}
+
+      <div className="categories-row">
+      <div className="category-card">
+      <div className="category-head">
+      <SpeakerOutlinedIcon className="category-icon" sx={{ fontSize: 90 }} />
       </div>
-      <Container className="poster">
-        <img
-          alt="sale_poster"
-          src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/cepc/med/may22/D45798145_IN_PC-Electronics-Mega-Electronics-days_1500x300.jpg"
-        />
-      </Container>
-      <div className="home_row_b">
-        {rowC.map((item, index) => {
-          return (
-            <Product
-              key={index}
-              product_id={item.product_id}
-              name={item.product_name}
-              rating={item.rating}
-              img={item.img_url}
-              price={item.price}
-            />
-          );
-        })}
+      <span>Amazon Devices</span>
       </div>
-      <Container className="poster">
-        <img
-          alt="sale_poster"
-          src="https://images-eu.ssl-images-amazon.com/images/G/31/img22/Fashion/Event/MFW_4/AFpage/Cashback/Eng/CB-PC-2.jpg"
-        />
-      </Container>
+      </div>
+
+      </div>
+      <div className="home-right">
+      hello
+      </div>
     </div>
   );
 };
 
 export default Home;
+
+//https://images-eu.ssl-images-amazon.com/images/G/31/img22/Events/Aug-ART-22/GW/Event/Hero/NTA/Aug_ART_PC_LIVE_now_UNREC_FDFO._CB630844079_.jpg
